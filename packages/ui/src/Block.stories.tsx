1319f272313f791b2182e98863c0260f67f236c6
lsj0202 <w1004123@gmail.com>
2024-03-18 00:02:13 +0900

    feat(ui): storybook ts -> css 변경된 스타일 적용

---
+++ packages/ui/src/Block.stories.tsx
@@ -0,0 +1,13 @@
+import { Block as _Block } from '@with/services';
+import '@with/services/style.css';
+
+export default {
+	title: 'Block',
+	component: _Block,
+	parameters: {
+		layout: 'centered',
+	},
+	tags: ['autodocs'],
+};
+
+export const BlockStory = {};
