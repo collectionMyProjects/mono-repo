5995500c51ab839250cf79891c8a01167770c6f4
lsj0202 <w1004123@gmail.com>
2024-03-20 01:05:25 +0900

    feat(client): @with/services 의존성 추가

--- apps/client/app/page.tsx
+++ apps/client/app/page.tsx
@@ -1,25 +1,9 @@
 import React from 'react';
 import '@with/foundation/themes.css';
 import '@with/foundation/globalStyle.css';
-import { Box, Flex } from '@with/services';
 
 const page = () => {
-	return (
-		<>
-			<Flex
-				color="pink"
-				style={{
-					width: '100%',
-					height: '100vh',
-				}}
-				align="center"
-				justify="center"
-			>
-				이상진
-			</Flex>
-			<Box color="blue">sdf</Box>
-		</>
-	);
+	return <></>;
 };
 
 export default page;
